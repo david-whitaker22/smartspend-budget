34fa89bae789214c3a8fa88bc9573a174b1a60a5
david-whitaker22 <dwhitakerponce@gmail.com>
2025-12-10 20:06:00 +0000

    Fix GitHub Pages SPA routing with 404 redirect and basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/smartspend-budget">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
